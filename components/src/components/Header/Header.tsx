9d1895398c6ad1060921cbbf640d068d7cc01a59
mldx <anton.zaprutski@gmail.com>
2023-03-18 16:43:51 +0300

    feat: add functional toggle active page in Header

--- components/src/components/Header/Header.tsx
+++ components/src/components/Header/Header.tsx
@@ -2,19 +2,37 @@ import React from 'react';
 import './Header.scss';
 import { Link } from 'react-router-dom';
 
-class Header extends React.Component {
+class Header extends React.Component<object, { isChangedPage: boolean }> {
   constructor(props: object) {
     super(props);
+    this.state = { isChangedPage: false };
   }
+
+  handleClick = () => {
+    this.setState({ isChangedPage: !this.state.isChangedPage });
+  };
+  toggleActivePageClass = (page: string) => {
+    const currentPage = window.location.pathname.split('/')[1];
+    return currentPage === page ? 'activePage' : '';
+  };
+
   render() {
     return (
-      <header>
+      <header className={'header'}>
         <nav className="navigate-container">
-          <Link className="nav-link" to="/about">
-            About
+          <Link
+            className={`navigate-link ${this.toggleActivePageClass('')}`}
+            to="/"
+            onClick={this.handleClick}
+          >
+            Home
           </Link>
-          <Link className="nav-link" to="/aboasdsuadt">
-            Error
+          <Link
+            className={`navigate-link ${this.toggleActivePageClass('about')}`}
+            to="/about"
+            onClick={this.handleClick}
+          >
+            About
           </Link>
         </nav>
       </header>
