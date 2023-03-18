b61fc091aadd3a8816abd9b9d46c33182012214f
mldx <anton.zaprutski@gmail.com>
2023-03-18 21:29:03 +0300

    feat: change Header component

--- components/src/components/Header/Header.tsx
+++ components/src/components/Header/Header.tsx
@@ -1,40 +1,12 @@
 import React from 'react';
 import './Header.scss';
-import { Link } from 'react-router-dom';
+import Navigate from '../Navigate/Navigate';
 
-class Header extends React.Component<object, { isChangedPage: boolean }> {
-  constructor(props: object) {
-    super(props);
-    this.state = { isChangedPage: false };
-  }
-
-  handleClick = () => {
-    this.setState({ isChangedPage: !this.state.isChangedPage });
-  };
-  toggleActivePageClass = (page: string) => {
-    const currentPage = window.location.pathname.split('/')[1];
-    return currentPage === page ? 'activePage' : '';
-  };
-
+class Header extends React.Component {
   render() {
     return (
       <header className={'header'}>
-        <nav className="navigate-container">
-          <Link
-            className={`navigate-link ${this.toggleActivePageClass('')}`}
-            to="/"
-            onClick={this.handleClick}
-          >
-            Home
-          </Link>
-          <Link
-            className={`navigate-link ${this.toggleActivePageClass('about')}`}
-            to="/about"
-            onClick={this.handleClick}
-          >
-            About
-          </Link>
-        </nav>
+        <Navigate></Navigate>
       </header>
     );
   }
