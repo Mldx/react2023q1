41361b22f319bcc8a2200ed996f0a832264e2d83
mldx <anton.zaprutski@gmail.com>
2023-03-18 21:29:32 +0300

    feat: add Navigate component

---
+++ components/src/components/Navigate/Navigate.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Navigate.scss';
+import { Link } from 'react-router-dom';
+
+class Navigate extends React.Component<object, { isChangedPage: boolean }> {
+  constructor(props: object) {
+    super(props);
+    this.state = { isChangedPage: false };
+  }
+
+  handleClick = () => {
+    this.setState({ isChangedPage: !this.state.isChangedPage });
+  };
+  toggleActivePageClass = (page: string) => {
+    const currentPage = window.location.pathname.split('/')[1];
+    return currentPage === page ? 'activePage' : '';
+  };
+
+  render() {
+    return (
+      <header className={'header'}>
+        <nav className="navigate-container">
+          <Link
+            className={`navigate-link ${this.toggleActivePageClass('')}`}
+            to="/"
+            onClick={this.handleClick}
+          >
+            Home
+          </Link>
+          <Link
+            className={`navigate-link ${this.toggleActivePageClass('about')}`}
+            to="/about"
+            onClick={this.handleClick}
+          >
+            About
+          </Link>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Navigate;
