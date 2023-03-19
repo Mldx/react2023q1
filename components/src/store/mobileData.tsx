779befea68c3d372506365aa9455f97e1e16e9b9
mldx <anton.zaprutski@gmail.com>
2023-03-19 13:09:11 +0300

    feat: add mobiles data

---
+++ components/src/store/mobileData.tsx
@@ -0,0 +1,115 @@
+import image1 from '../assets/1.webp';
+import image2 from '../assets/2.webp';
+import image3 from '../assets/3.webp';
+import image4 from '../assets/4.webp';
+import image5 from '../assets/5.webp';
+import image6 from '../assets/6.webp';
+import image7 from '../assets/7.webp';
+import image8 from '../assets/8.webp';
+import image9 from '../assets/9.webp';
+import image10 from '../assets/10.webp';
+
+const mobileData = [
+  {
+    id: '1',
+    model: 'Iphone 14 pro max',
+    color: 'white',
+    ram: '4',
+    screen: '6.7',
+    cameras: '3',
+    price: '1200',
+    image: image1,
+  },
+  {
+    id: '2',
+    model: 'Iphone 12',
+    color: 'red',
+    ram: '4',
+    screen: '6.1',
+    cameras: '3',
+    price: '800',
+    image: image2,
+  },
+  {
+    id: '3',
+    model: 'Samsung Galaxy S21',
+    color: 'violet',
+    ram: '8',
+    screen: '6.2',
+    cameras: '3',
+    price: '920',
+    image: image3,
+  },
+  {
+    id: '4',
+    model: 'Xiaomi Mi 10T',
+    color: 'grey',
+    ram: '6',
+    screen: '6.67',
+    cameras: '4',
+    price: '420',
+    image: image4,
+  },
+  {
+    id: '5',
+    model: 'Iphone XR',
+    color: 'yellow',
+    ram: '3',
+    screen: '6.1',
+    cameras: '1',
+    price: '540',
+    image: image5,
+  },
+  {
+    id: '6',
+    model: 'Iphone 12',
+    color: 'violet',
+    ram: '4',
+    screen: '6.1',
+    cameras: '3',
+    price: '800',
+    image: image6,
+  },
+  {
+    id: '7',
+    model: 'Samsung Galaxy A33',
+    color: 'white',
+    ram: '6',
+    screen: '6.4',
+    cameras: '4',
+    price: '350',
+    image: image7,
+  },
+  {
+    id: '8',
+    model: 'OPPO Find X5 Pro',
+    color: 'black',
+    ram: '12',
+    screen: '6.7',
+    cameras: '2',
+    price: '740',
+    image: image8,
+  },
+  {
+    id: '9',
+    model: 'Oppo A54',
+    color: 'grey',
+    ram: '6',
+    screen: '6.49',
+    cameras: '3',
+    price: '650',
+    image: image9,
+  },
+  {
+    id: '10',
+    model: 'Oppo Find X3 Neo',
+    color: 'grey',
+    ram: '12',
+    screen: '6.55',
+    cameras: '3',
+    price: '700',
+    image: image10,
+  },
+];
+
+export default mobileData;
