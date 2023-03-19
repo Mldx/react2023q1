d46c685b17919da0be4469d5eee3f38e42687222
mldx <anton.zaprutski@gmail.com>
2023-03-19 11:48:58 +0300

    fix: fix Card component

--- components/src/components/Card/Card.tsx
+++ components/src/components/Card/Card.tsx
@@ -1,34 +1,63 @@
 import React from 'react';
 import './Card.scss';
-import pathImage from '../../assets/12.webp';
 
-class Card extends React.Component {
+class Card extends React.Component<{
+  data: {
+    id: string;
+    model: string;
+    color: string;
+    ram: string;
+    screen: string;
+    cameras: string;
+    price: string;
+    image: string;
+  };
+}> {
+  constructor(props: {
+    data: {
+      id: string;
+      model: string;
+      color: string;
+      ram: string;
+      screen: string;
+      cameras: string;
+      price: string;
+      image: string;
+    };
+  }) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="card-container">
         <div className="image-container">
-          <img src={pathImage} alt="mobile image" />
+          <img src={this.props.data.image} alt="mobile image" />
         </div>
         <div className="card-container__properties">
           <div>
             <span className="properties-name">Model:</span>
-            <span className="properties-value">Iphone 14 pro max</span>
+            <span className="properties-value">{this.props.data.model}</span>
           </div>
           <div>
             <span className="properties-name">Color:</span>
-            <span className="properties-value">white</span>
+            <span className="properties-value">{this.props.data.color}</span>
           </div>
           <div>
             <span className="properties-name">Ram:</span>
-            <span className="properties-value">4 gb</span>
+            <span className="properties-value">{this.props.data.ram} gb</span>
           </div>
           <div>
             <span className="properties-name">Screen:</span>
-            <span className="properties-value">5.1 inch</span>
+            <span className="properties-value">{this.props.data.screen} inch</span>
           </div>
           <div>
             <span className="properties-name">Cameras:</span>
-            <span className="properties-value">3</span>
+            <span className="properties-value">{this.props.data.cameras}</span>
+          </div>
+          <div>
+            <span className="properties-name">Price:</span>
+            <span className="properties-value">{this.props.data.price} $</span>
           </div>
         </div>
       </div>
