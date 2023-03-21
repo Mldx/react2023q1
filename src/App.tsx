4d3ce11ad2fbe7ba39d6fcbd005295e8b99c9f60
mldx <anton.zaprutski@gmail.com>
2023-03-22 01:21:17 +0300

    feat: add FormPage component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 
-import ErrorPage from './components/ErrorPage/ErrorPage';
 import AboutPage from './components/AboutPage/AboutPage';
-import Layout from './components/Layout/Layout';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import FormPage from './components/FormPage/FormPage';
 import HomePage from './components/HomePage/HomePage';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />}></Route>
           <Route path="/about" element={<AboutPage />}></Route>
+          <Route path="/form" element={<FormPage />}></Route>
         </Route>
         <Route path="*" element={<ErrorPage />}></Route>
       </Routes>
---
+++ src/components/FormPage/FormPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './FormPage.scss';
+import SearchBar from '../SearchBar/SearchBar';
+
+class FormPage extends React.Component {
+  render() {
+    return (
+      <div className="forms-page">
+        <h1>Form page</h1>
+      </div>
+    );
+  }
+}
+
+export default FormPage;
