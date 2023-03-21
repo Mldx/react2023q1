f8a745be1af526ffb56e83414ff0c38ece619abf
mldx <anton.zaprutski@gmail.com>
2023-03-21 23:55:00 +0300

    feat: setup restrict to build with eslint error

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,25 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      ...eslint(),
+      apply: 'build',
+    },
+    {
+      ...eslint({
+        failOnWarning: false,
+        failOnError: false,
+      }),
+      apply: 'serve',
+      enforce: 'post',
+    },
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
