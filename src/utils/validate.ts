a2d6b3131be3e15cc813f7a82b25614945c32b36
mldx <anton.zaprutski@gmail.com>
2023-03-25 02:44:13 +0300

    feat: add regExp constant

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -16,11 +16,13 @@ const GENDER_IS_MISSING = 'Please choose the gender';
 const FILE_IS_MISSING = 'Please add your avatar';
 const FILE_IS_INCORRECT = 'Please add an image file';
 
+const REG_EXP_CAPITALIZE_EN_RU = new RegExp(/[A-ZА-Я]/);
+
 const checkName = (value: string | undefined) => {
   let errorMessage = '';
   if (!value) {
     errorMessage = NAME_IS_MISSING;
-  } else if (!/[A-Z]/.test(value[0])) {
+  } else if (!REG_EXP_CAPITALIZE_EN_RU.test(value[0])) {
     errorMessage = NAME_IS_NOT_CAPITALIZE;
   }
   return errorMessage;
@@ -30,7 +32,7 @@ const checkSurname = (value: string | undefined) => {
   let errorMessage = '';
   if (!value) {
     errorMessage = SURNAME_IS_MISSING;
-  } else if (!/[A-Z]/.test(value[0])) {
+  } else if (!REG_EXP_CAPITALIZE_EN_RU.test(value[0])) {
     errorMessage = SURNAME_IS_NOT_CAPITALIZE;
   }
   return errorMessage;
