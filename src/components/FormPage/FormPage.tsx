c565777c320d0bfeb80c69cdbd0ae00f07aa7d52
mldx <anton.zaprutski@gmail.com>
2023-03-25 02:51:33 +0300

    feat: some change FormPage component

--- src/components/FormPage/FormPage.tsx
+++ src/components/FormPage/FormPage.tsx
@@ -3,32 +3,7 @@ import './FormPage.scss';
 
 class FormPage extends React.Component {
   render() {
-    return (
-      <div className="forms-page">
-        <form className="ordering-form">
-          <h1 className="ordering-title">Ordering form</h1>
-          <input type="text" />
-
-          <input type="date" />
-
-          <select>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-          </select>
-
-          <input type="checkbox" />
-
-          <input type="radio" name="test" />
-          <input type="radio" name="test" />
-
-          <input type="file" />
-
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
+    return <div className="form-page"></div>;
   }
 }
-
 export default FormPage;
