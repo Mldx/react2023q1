9e65ac0bb0acf29a1d7cc14422a2abef9d3ec2b1
mldx <anton.zaprutski@gmail.com>
2023-03-25 18:40:36 +0300

    feat: add style and logic in OrderCard component

--- src/components/OrderingForm/OrderCard/OrderCard.tsx
+++ src/components/OrderingForm/OrderCard/OrderCard.tsx
@@ -13,7 +13,37 @@ class OrderCard extends React.Component<{ data: IOrderingFormData }> {
       <div className="order-cards-container">
         <div className="order-card">
           {data.avatar ? <img src={URL.createObjectURL(data.avatar)} alt="avatar" /> : <></>}
-          <p>{data.name + ' - ' + data.surname}</p>
+          <div className="order-card-info">
+            <div className="info-block block-user-info">
+              <span className="info-block__title">User info:</span>
+              <div>
+                <span className="info-block__field-name">Full name: </span>
+                {`${data.name} ${data.surname}`}
+              </div>
+              <div>
+                <span className="info-block__field-name">Gender: </span>
+                {data.gender}
+              </div>
+            </div>
+            <div className="info-block block-delivery-info">
+              <span className="info-block__title">Delivery info:</span>
+              <div>
+                <span className="info-block__field-name">City: </span>
+                {data.cityDelivery}
+              </div>
+              <div>
+                <span className="info-block__field-name">Date: </span>
+                {data.dateDelivery}
+              </div>
+            </div>
+            <div className="info-block block-others">
+              <span className="info-block__title">Others:</span>
+              <div>
+                <span className="info-block__field-name">Personal data: </span>
+                {data.personalData ? 'agreed to use' : ''}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
