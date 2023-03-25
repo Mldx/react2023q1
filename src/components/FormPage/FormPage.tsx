ec09c04fdbd8e7ab400e3cf30da774d9f4f4e7c3
mldx <anton.zaprutski@gmail.com>
2023-03-25 20:33:50 +0300

    feat: add some style and change child component

--- src/components/FormPage/FormPage.tsx
+++ src/components/FormPage/FormPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './FormPage.scss';
+import OrderingForm from '../OrderingForm/OrderingForm';
 
 class FormPage extends React.Component {
   render() {
-    return <div className="form-page"></div>;
+    return (
+      <div className="form-page">
+        <OrderingForm />
+      </div>
+    );
   }
 }
 export default FormPage;
