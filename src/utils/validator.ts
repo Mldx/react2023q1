3f40b640442c9600b5130c7fcce03e6514615210
mldx <anton.zaprutski@gmail.com>
2023-03-25 18:06:42 +0300

    feat: add validator logic to check A-z and А-я in name and surname fields

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,8 +1,12 @@
 const NAME_IS_MISSING = 'Please enter your name';
 const NAME_IS_NOT_CAPITALIZE = 'Please enter your name with capital letters';
+const NAME_IS_INCORRECT_LENGTH = 'Please enter your name with 3-16 letters';
+const NAME_IS_INCORRECT = 'Please enter your name only with EN or RU letters';
 
 const SURNAME_IS_MISSING = 'Please enter your surname';
 const SURNAME_IS_NOT_CAPITALIZE = 'Please enter your surname with capital letters';
+const SURNAME_IS_INCORRECT_LENGTH = 'Please enter your surname with 3-16 letters';
+const SURNAME_IS_INCORRECT = 'Please enter your surname only with EN or RU letters';
 
 const DELIVERY_DATE_IS_MISSING = 'Please choose the delivery date';
 const DELIVERY_DATE_INCORRECT = 'Please choose the correct date';
@@ -17,11 +21,16 @@ const FILE_IS_MISSING = 'Please add your avatar';
 const FILE_IS_INCORRECT = 'Please add an image file';
 
 const REG_EXP_CAPITALIZE_EN_RU = new RegExp(/[A-ZА-Я]/);
+const REG_EXP_EN_RU = new RegExp(/[^A-ZА-Я]/i);
 
 const checkName = (value: string | undefined) => {
   let errorMessage = '';
   if (!value) {
     errorMessage = NAME_IS_MISSING;
+  } else if (REG_EXP_EN_RU.test(value)) {
+    errorMessage = NAME_IS_INCORRECT;
+  } else if (value.length < 3 || value.length > 16) {
+    errorMessage = NAME_IS_INCORRECT_LENGTH;
   } else if (!REG_EXP_CAPITALIZE_EN_RU.test(value[0])) {
     errorMessage = NAME_IS_NOT_CAPITALIZE;
   }
@@ -32,6 +41,10 @@ const checkSurname = (value: string | undefined) => {
   let errorMessage = '';
   if (!value) {
     errorMessage = SURNAME_IS_MISSING;
+  } else if (REG_EXP_EN_RU.test(value)) {
+    errorMessage = SURNAME_IS_INCORRECT;
+  } else if (value.length < 3 || value.length > 16) {
+    errorMessage = SURNAME_IS_INCORRECT_LENGTH;
   } else if (!REG_EXP_CAPITALIZE_EN_RU.test(value[0])) {
     errorMessage = SURNAME_IS_NOT_CAPITALIZE;
   }
