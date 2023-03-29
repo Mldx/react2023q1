972b9c16da1945b53367536b32528e8da885df55
mldx <anton.zaprutski@gmail.com>
2023-03-30 01:37:48 +0300

    refactor: change SearchBar to functional component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,32 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './SearchBar.scss';
 
-class SearchBar extends React.Component<object, { value: string }> {
-  constructor(props: object) {
-    super(props);
-    this.state = { value: localStorage.getItem('searchBarValue') || '' };
-  }
+function SearchBar() {
+  const initialValue = localStorage.getItem('searchBarValue') || '';
+  const [value, setValue] = useState(initialValue);
 
-  componentWillUnmount() {
-    localStorage.setItem('searchBarValue', this.state.value || '');
-  }
-
-  handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ value: event.target.value });
+  useEffect(() => {
+    return () => {
+      localStorage.setItem('searchBarValue', value);
+    };
+  }, [value]);
+  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
   };
 
-  render() {
-    return (
-      <div className="searchBar">
-        <input
-          className="searchBar__input"
-          type={'text'}
-          value={this.state.value}
-          onChange={this.handleChangeValue}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="searchBar">
+      <input
+        className="searchBar__input"
+        type={'text'}
+        value={value}
+        onChange={handleChangeValue}
+      />
+    </div>
+  );
 }
 
 export default SearchBar;
