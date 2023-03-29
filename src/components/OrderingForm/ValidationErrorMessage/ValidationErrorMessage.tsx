5bfd538b05a08dfb6c8a5083ec2b8b83c765014a
mldx <anton.zaprutski@gmail.com>
2023-03-29 06:43:34 +0300

    refactor: remove extra code

--- src/components/OrderingForm/ValidationErrorMessage/ValidationErrorMessage.tsx
+++ src/components/OrderingForm/ValidationErrorMessage/ValidationErrorMessage.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import './ValidationErrorMessage.scss';
 
-class ValidationErrorMessage extends React.Component<{ errorMessage: string | undefined }> {
-  constructor(props: { errorMessage: string }) {
+class ValidationErrorMessage extends React.Component<{
+  errorMessage: string | undefined;
+}> {
+  constructor(props: { errorMessage: string | undefined }) {
     super(props);
   }
 
   render() {
     const { errorMessage } = this.props;
-    return (
-      errorMessage && (
-        <>
-          <span className="validation-error">{errorMessage}</span>
-        </>
-      )
-    );
+    return errorMessage && <span className="validation-error">{errorMessage}</span>;
   }
 }
 
