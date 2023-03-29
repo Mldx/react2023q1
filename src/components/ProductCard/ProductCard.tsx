fa9511cfe7057bfd4f23a7df75e805d61574111d
mldx <anton.zaprutski@gmail.com>
2023-03-30 02:42:01 +0300

    refactor: change ProductCard to functional component

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -3,47 +3,45 @@ import './ProductCard.scss';
 import AddButton from './AddButton/AddButton';
 import { IMobileInfo } from '../../types/types';
 
-class ProductCard extends React.Component<{ data: IMobileInfo }> {
-  constructor(props: { data: IMobileInfo }) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="product-card">
-        <div className="product-card__image-box">
-          <img src={this.props.data.image} alt="mobile image" />
-        </div>
-        <div className="product-card__properties">
-          <div>
-            <span className="properties-name">Model:</span>
-            <span className="properties-value">{this.props.data.model}</span>
-          </div>
-          <div>
-            <span className="properties-name">Color:</span>
-            <span className="properties-value">{this.props.data.color}</span>
-          </div>
-          <div>
-            <span className="properties-name">Ram:</span>
-            <span className="properties-value">{this.props.data.ram} gb</span>
-          </div>
-          <div>
-            <span className="properties-name">Screen:</span>
-            <span className="properties-value">{this.props.data.screen} inch</span>
-          </div>
-          <div>
-            <span className="properties-name">Cameras:</span>
-            <span className="properties-value">{this.props.data.cameras}</span>
-          </div>
-          <div>
-            <span className="properties-name">Price:</span>
-            <span className="properties-value">{this.props.data.price} $</span>
-          </div>
-          <AddButton />
-        </div>
-      </div>
-    );
-  }
+interface IProductCardProps {
+  data: IMobileInfo;
 }
 
+function ProductCard(props: IProductCardProps) {
+  const { image, model, color, ram, screen, cameras, price } = props.data;
+  return (
+    <div className="product-card">
+      <div className="product-card__image-box">
+        <img src={image} alt="mobile image" />
+      </div>
+      <div className="product-card__properties">
+        <div>
+          <span className="properties-name">Model:</span>
+          <span className="properties-value">{model}</span>
+        </div>
+        <div>
+          <span className="properties-name">Color:</span>
+          <span className="properties-value">{color}</span>
+        </div>
+        <div>
+          <span className="properties-name">Ram:</span>
+          <span className="properties-value">{ram} gb</span>
+        </div>
+        <div>
+          <span className="properties-name">Screen:</span>
+          <span className="properties-value">{screen} inch</span>
+        </div>
+        <div>
+          <span className="properties-name">Cameras:</span>
+          <span className="properties-value">{cameras}</span>
+        </div>
+        <div>
+          <span className="properties-name">Price:</span>
+          <span className="properties-value">{price} $</span>
+        </div>
+        <AddButton />
+      </div>
+    </div>
+  );
+}
 export default ProductCard;
