b2b4334946bfa331338de3c15e61a7e16a979ba3
mldx <anton.zaprutski@gmail.com>
2023-03-30 03:34:53 +0300

    refactor: change HomePage to functional component

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -4,15 +4,13 @@ import SearchBar from '../SearchBar/SearchBar';
 import ProductCardContainer from '../ProductCardContainer/ProductCardContainer';
 import mobileData from '../../store/mobileData';
 
-class HomePage extends React.Component {
-  render() {
-    return (
-      <div className="home-page">
-        <SearchBar></SearchBar>
-        <ProductCardContainer dataList={mobileData}></ProductCardContainer>
-      </div>
-    );
-  }
+function HomePage() {
+  return (
+    <div className="home-page">
+      <SearchBar></SearchBar>
+      <ProductCardContainer dataList={mobileData}></ProductCardContainer>
+    </div>
+  );
 }
 
 export default HomePage;
