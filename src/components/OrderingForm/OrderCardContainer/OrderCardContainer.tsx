c4d975f09b5cd513305c394aeb9bfc92c41aabfb
mldx <anton.zaprutski@gmail.com>
2023-03-30 04:42:03 +0300

    refactor: change OrderCardContainer to functional component

--- src/components/OrderingForm/OrderCardContainer/OrderCardContainer.tsx
+++ src/components/OrderingForm/OrderCardContainer/OrderCardContainer.tsx
@@ -3,22 +3,22 @@ import './OrderCardContainer.scss';
 import { IOrderingFormData } from '../../../types/types';
 import OrderCard from '../OrderCard/OrderCard';
 
-class OrderCardContainer extends React.Component<{ dataList: IOrderingFormData[] }> {
-  constructor(props: { dataList: IOrderingFormData[] }) {
-    super(props);
-  }
+interface IOrderCardContainerProps {
+  dataList: IOrderingFormData[];
+}
 
-  render() {
-    return this.props.dataList.length ? (
-      <div className="order-cards-container">
-        {this.props.dataList.map((data, index) => (
-          <OrderCard data={data} key={index} />
-        ))}
-      </div>
-    ) : (
-      <></>
-    );
-  }
+function OrderCardContainer(props: IOrderCardContainerProps) {
+  const { dataList } = props;
+
+  return dataList.length ? (
+    <div className="order-cards-container">
+      {dataList.map((data, index) => (
+        <OrderCard data={data} key={index} />
+      ))}
+    </div>
+  ) : (
+    <></>
+  );
 }
 
 export default OrderCardContainer;
