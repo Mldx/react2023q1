f284532ef0b5e75e4c11dbfe171c52b47cf00203
mldx <anton.zaprutski@gmail.com>
2023-03-31 07:11:27 +0300

    feat: rewrite OrderingForm component with react-hook-form

--- src/components/OrderingForm/OrderingForm.tsx
+++ src/components/OrderingForm/OrderingForm.tsx
@@ -1,152 +1,141 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './OrderingForm.scss';
-import './OrderCreatedWindow/OrderCreatedWindow.scss';
-import { IOrderingFormData, IOrderingFormErrors, IOrderingFormState } from '../../types/types';
-import validate from '../../utils/validator';
+import { IOrderingFormData } from '../../types/types';
 import ValidationErrorMessage from './ValidationErrorMessage/ValidationErrorMessage';
 import OrderCardContainer from './OrderCardContainer/OrderCardContainer';
 import OrderCreatedWindow from './OrderCreatedWindow/OrderCreatedWindow';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import validator from '../../utils/validator';
+import getFirstFileFromList from '../../utils/getFirstFileFromList';
 
-class OrderingForm extends React.Component<object, IOrderingFormState> {
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      formErrors: {},
-      orderList: [],
-      orderCreated: false,
-    };
-  }
+interface IOrderingForm {
+  name: string;
+  surname: string;
+  dateDelivery: string;
+  cityDelivery: string;
+  personalData: boolean;
+  gender: string;
+  avatar: FileList;
+}
 
-  refList = {
-    form: React.createRef<HTMLFormElement>(),
-    inputName: React.createRef<HTMLInputElement>(),
-    inputSurname: React.createRef<HTMLInputElement>(),
-    inputDateDelivery: React.createRef<HTMLInputElement>(),
-    selectCityDelivery: React.createRef<HTMLSelectElement>(),
-    inputPersonalData: React.createRef<HTMLInputElement>(),
-    inputRadioGenderList: [
-      React.createRef<HTMLInputElement>(),
-      React.createRef<HTMLInputElement>(),
-    ],
-    inputAvatar: React.createRef<HTMLInputElement>(),
+function OrderingForm() {
+  const DELIVERY_CITIES = ['', 'Ankara', 'Minsk', 'Tbilisi', 'Vilnius', 'Warsaw'];
+  const GENDERS = ['male', 'female'];
+
+  const [dataOrders, setDataOrders] = useState<IOrderingFormData[]>([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+  } = useForm<IOrderingForm>({ mode: 'onSubmit', reValidateMode: 'onSubmit' });
+
+  const onSubmit: SubmitHandler<IOrderingForm> = (data) => {
+    const avatarObject = getFirstFileFromList(data.avatar);
+    setDataOrders([...dataOrders, { ...data, avatar: avatarObject }]);
+    reset();
   };
 
-  formIsValid = () => {
-    const dataFromForm = this.getDataFromForm();
-    const errors: IOrderingFormErrors = {};
+  return (
+    <>
+      <form className="ordering-form" onSubmit={handleSubmit(onSubmit)} noValidate={true}>
+        <label>
+          Name:
+          <input
+            type="text"
+            autoComplete="new-password"
+            {...register('name', {
+              validate: (nameValue) => validator.name(nameValue),
+            })}
+          />
+          <ValidationErrorMessage errorMessage={errors.name?.message} />
+        </label>
 
-    errors.name = validate.name(dataFromForm.name);
-    errors.surname = validate.surname(dataFromForm.surname);
-    errors.dateDelivery = validate.dateDelivery(dataFromForm.dateDelivery);
-    errors.cityDelivery = validate.cityDelivery(dataFromForm.cityDelivery);
-    errors.personalData = validate.personalData(dataFromForm.personalData);
-    errors.gender = validate.gender(dataFromForm.gender);
-    errors.avatar = validate.avatar(dataFromForm.avatar);
+        <label>
+          Surname:
+          <input
+            type="text"
+            autoComplete="new-password"
+            {...register('surname', {
+              validate: (surnameValue) => validator.surname(surnameValue),
+            })}
+          />
+          <ValidationErrorMessage errorMessage={errors.surname?.message} />
+        </label>
 
-    this.setState({ formErrors: errors });
-    return !Object.values(errors).some((err) => err.length > 0);
-  };
+        <label>
+          Delivery date:
+          <input
+            type="date"
+            {...register('dateDelivery', {
+              validate: (dateDeliveryValue) => validator.dateDelivery(dateDeliveryValue),
+            })}
+          />
+          <ValidationErrorMessage errorMessage={errors.dateDelivery?.message} />
+        </label>
 
-  handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (this.formIsValid()) {
-      this.setState({
-        orderList: [...this.state.orderList, this.getDataFromForm()],
-        orderCreated: true,
-      });
-      this.refList.form.current?.reset();
-    }
-  };
+        <label>
+          Delivery city:
+          <select
+            {...register('cityDelivery', {
+              validate: (cityDeliveryValue) => validator.cityDelivery(cityDeliveryValue),
+            })}
+          >
+            {DELIVERY_CITIES.map((city) => (
+              <option key={city}>{city}</option>
+            ))}
+          </select>
+          <ValidationErrorMessage errorMessage={errors.cityDelivery?.message} />
+        </label>
 
-  private getDataFromForm = (): IOrderingFormData => {
-    return {
-      name: this.refList.inputName.current?.value.trim(),
-      surname: this.refList.inputSurname.current?.value.trim(),
-      dateDelivery: this.refList.inputDateDelivery.current?.value,
-      cityDelivery: this.refList.selectCityDelivery.current?.value,
-      personalData: this.refList.inputPersonalData.current?.checked,
-      gender: this.refList.inputRadioGenderList.find((el) => el.current?.checked)?.current?.value,
-      avatar: this.refList.inputAvatar.current?.files?.[0],
-    };
-  };
+        <label className="personal-data-box">
+          <input
+            type="checkbox"
+            {...register('personalData', {
+              validate: (personalDataValue) => validator.personalData(personalDataValue),
+            })}
+          />
+          I allow to use my personal data:
+          <ValidationErrorMessage errorMessage={errors.personalData?.message} />
+        </label>
 
-  render() {
-    const DELIVERY_CITIES = ['', 'Ankara', 'Minsk', 'Tbilisi', 'Vilnius', 'Warsaw'];
-    const GENDERS = ['male', 'female'];
-    return (
-      <>
-        <form
-          className="ordering-form"
-          onSubmit={this.handleSubmit}
-          ref={this.refList.form}
-          noValidate={true}
-        >
-          <label>
-            Name:
-            <input type="text" ref={this.refList.inputName} autoComplete="new-password" />
-            <ValidationErrorMessage errorMessage={this.state.formErrors.name} />
-          </label>
-
-          <label>
-            Surname:
-            <input type="text" ref={this.refList.inputSurname} autoComplete="new-password" />
-            <ValidationErrorMessage errorMessage={this.state.formErrors.surname} />
-          </label>
-
-          <label>
-            Delivery date:
-            <input type="date" ref={this.refList.inputDateDelivery} />
-            <ValidationErrorMessage errorMessage={this.state.formErrors.dateDelivery} />
-          </label>
-
-          <label>
-            Delivery city:
-            <select ref={this.refList.selectCityDelivery}>
-              {DELIVERY_CITIES.map((city) => (
-                <option key={city}>{city}</option>
-              ))}
-            </select>
-            <ValidationErrorMessage errorMessage={this.state.formErrors.cityDelivery} />
-          </label>
-
-          <label className="personal-data-box">
-            <input type="checkbox" ref={this.refList.inputPersonalData} />I allow to use my personal
-            data:
-            <ValidationErrorMessage errorMessage={this.state.formErrors.personalData} />
-          </label>
-
-          <div className="gender-box">
-            Gender:
-            <div className="radio-options-box">
-              {GENDERS.map((gender, index) => (
-                <label key={gender}>
-                  <input
-                    type="radio"
-                    ref={this.refList.inputRadioGenderList[index]}
-                    name="gender"
-                    value={gender}
-                  />
-                  {gender}
-                </label>
-              ))}
-              <ValidationErrorMessage errorMessage={this.state.formErrors.gender} />
-            </div>
+        <div className="gender-box">
+          Gender:
+          <div className="radio-options-box">
+            {GENDERS.map((gender) => (
+              <label key={gender}>
+                <input
+                  type="radio"
+                  {...register('gender', {
+                    validate: (genderValue) => validator.gender(genderValue),
+                  })}
+                  name="gender"
+                  value={gender}
+                />
+                {gender}
+              </label>
+            ))}
+            <ValidationErrorMessage errorMessage={errors.gender?.message} />
           </div>
+        </div>
 
-          <label className="input-avatar-box">
-            Your avatar:
-            <input type="file" ref={this.refList.inputAvatar} accept="image/*" />
-            <ValidationErrorMessage errorMessage={this.state.formErrors.avatar} />
-          </label>
-          <button type="submit">Submit</button>
-        </form>
-        <OrderCardContainer dataList={this.state.orderList} />
-        {this.state.orderCreated && (
-          <OrderCreatedWindow onAnimationEnd={() => this.setState({ orderCreated: false })} />
-        )}
-      </>
-    );
-  }
+        <label className="input-avatar-box">
+          Your avatar:
+          <input
+            type="file"
+            accept="image/*"
+            {...register('avatar', {
+              validate: (fileList) => validator.avatar(fileList[0]),
+            })}
+          />
+          <ValidationErrorMessage errorMessage={errors.avatar?.message} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <OrderCardContainer dataList={dataOrders} />
+      {isSubmitSuccessful && <OrderCreatedWindow />}
+    </>
+  );
 }
 
 export default OrderingForm;
