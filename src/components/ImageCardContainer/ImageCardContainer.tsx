a93951bcb2476fec8efb03362a93f742251e1075
mldx <anton.zaprutski@gmail.com>
2023-04-01 12:16:48 +0300

    feat: add ImageCardContainer component

---
+++ src/components/ImageCardContainer/ImageCardContainer.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import './ImageCardContainer.scss';
+import { createApi } from 'unsplash-js';
+import { ApiResponse } from 'unsplash-js/dist/helpers/response';
+import { Photos } from 'unsplash-js/dist/methods/search/types/response';
+import ImageCard from '../ImageCard/ImageCard';
+import LoadingScreen from '../LoadingScreen/LoadingScreen';
+
+const api = createApi({
+  accessKey: 'xmx-M-AJ0UbvjxTASHIBeoh8WtViYJw0YiMzsTBbJwE',
+});
+
+function ImageCardContainer(props: { currentQuery: string }) {
+  const [data, setData] = useState<ApiResponse<Photos> | null>(null);
+  useEffect(() => {
+    setData(null);
+    api.search
+      .getPhotos({
+        query: props.currentQuery,
+        orientation: 'portrait',
+        perPage: 6,
+        page: 1,
+      })
+      .then((result) => {
+        setData(result);
+      });
+  }, [props.currentQuery]);
+
+  if (!data) {
+    return <LoadingScreen />;
+  }
+
+  if (data.errors) {
+    return (
+      <div className="image_card-error_message">
+        <div>{data.errors[0]}</div>
+      </div>
+    );
+  }
+
+  if (!data.response?.results.length) {
+    return <h1>Not found 😞</h1>;
+  }
+
+  return (
+    <div className="image-cards-container">
+      {data.response.results.map((photo) => (
+        <ImageCard {...photo} key={photo.id}></ImageCard>
+      ))}
+    </div>
+  );
+}
+
+export default ImageCardContainer;
