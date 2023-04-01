8ef63799138b8c21a039947e599166ecd2f278af
mldx <anton.zaprutski@gmail.com>
2023-04-01 20:49:51 +0300

    feat: add ImageCardPopup component

---
+++ src/components/ImageCard/ImageCardPopup/ImageCardPopup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ImageCardPopup.scss';
+
+function ImageCardPopup() {
+  return (
+    <div className="image-card_popup-container">
+      <div className="popup-container_image_box">{/*<img src={urls.regular} alt="photo" />*/}</div>
+    </div>
+  );
+}
+
+export default ImageCardPopup;
