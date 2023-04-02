174c8a5b5f002757d36c8916c53b858480335a84
mldx <anton.zaprutski@gmail.com>
2023-04-02 03:46:19 +0300

    feat: change some logic and style ImageCard component

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -1,51 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ImageCard.scss';
 import { Basic } from 'unsplash-js/dist/methods/photos/types';
 import LikeIcon from '../LikeIcon/LikeIcon';
+import ImageCardPopup from './ImageCardPopup/ImageCardPopup';
 
 function ImageCard(photo: Basic) {
+  const [popupIsActive, setPopupIsActive] = useState(false);
   const {
     urls,
     user: { name, instagram_username, twitter_username },
   } = photo;
 
-  return (
-    <div className="image-card_container">
-      <div className="image-card_box">
-        <img src={urls.regular} alt="photo" />
-      </div>
-      <div className="image-card_info">
-        <span className="image-card_info-photographer">Photographer: {name}</span>
+  const togglePopup = (e: React.MouseEvent<HTMLElement>) => {
+    if (!(e.target instanceof HTMLAnchorElement)) {
+      setPopupIsActive(!popupIsActive);
+    }
+  };
 
-        {instagram_username && (
-          <span>
-            Instagram:
-            <a
-              href={`https://www.instagram.com/${instagram_username}`}
-              target="_blank"
-              rel="noreferrer"
-            >
-              &#32;@{instagram_username}
-            </a>
+  return (
+    <>
+      <div className="image-card_container" onClick={togglePopup}>
+        <div className="image-card_box">
+          <img src={urls.small} alt="photo" draggable="false" />
+        </div>
+        <div className="image-card_info">
+          <span className="image-card_info-photographer">Photographer: {name}</span>
+
+          {instagram_username && (
+            <span>
+              Instagram:
+              <a
+                href={`https://www.instagram.com/${instagram_username}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                &#32;@{instagram_username}
+              </a>
+            </span>
+          )}
+          {twitter_username && (
+            <span>
+              Twitter:
+              <a
+                href={`https://www.twitter.com/${twitter_username}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                &#32;@{twitter_username}
+              </a>
+            </span>
+          )}
+          <span className="image-card_like">
+            {photo.likes} <LikeIcon />
           </span>
-        )}
-        {twitter_username && (
-          <span>
-            Twitter:
-            <a
-              href={`https://www.twitter.com/${twitter_username}`}
-              target="_blank"
-              rel="noreferrer"
-            >
-              &#32;@{twitter_username}
-            </a>
-          </span>
-        )}
-        <span className="image-card_like">
-          {photo.likes} <LikeIcon />
-        </span>
+        </div>
       </div>
-    </div>
+      {popupIsActive && <ImageCardPopup func={togglePopup} photo={photo} />}
+    </>
   );
 }
 
