7e87339100be73cc88c5c76af8b0c82f2083678e
mldx <anton.zaprutski@gmail.com>
2023-04-02 03:48:00 +0300

    feat: add some styles in ImageCardPopup component

--- src/components/ImageCard/ImageCardPopup/ImageCardPopup.tsx
+++ src/components/ImageCard/ImageCardPopup/ImageCardPopup.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './ImageCardPopup.scss';
+import { Basic } from 'unsplash-js/dist/methods/photos/types';
 
-function ImageCardPopup() {
+interface IImageCardPopupProps {
+  func: (e: React.MouseEvent<HTMLElement>) => void;
+  photo: Basic;
+}
+
+function ImageCardPopup(props: IImageCardPopupProps) {
   return (
     <div className="image-card_popup-container">
-      <div className="popup-container_image_box">{/*<img src={urls.regular} alt="photo" />*/}</div>
+      <div className="image-card_popup-box">
+        <div className="popup-container_image-box">
+          <img src={props.photo.urls.regular} alt="photo" draggable="false" />
+        </div>
+        <div className="popup-container_info-box"></div>
+        <button className="popup-box_close-button" onClick={(e) => props.func(e)}></button>
+      </div>
+      <div className="image-card_popup-modal" onClick={(e) => props.func(e)}></div>
     </div>
   );
 }
