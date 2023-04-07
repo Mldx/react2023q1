19405296e7a45149d51fb525aa693afb07237bd9
mldx <anton.zaprutski@gmail.com>
2023-04-07 08:21:46 +0300

    feat: add redux in app

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Layout from './components/Layout/Layout';
 import { AboutPage, ErrorPage, FormPage, HomePage } from './components/pages';
-import AppProvider from './store/store';
 
 function App() {
   return (
-    <AppProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />}></Route>
-          <Route path="/about" element={<AboutPage />}></Route>
-          <Route path="/form" element={<FormPage />}></Route>
-        </Route>
-        <Route path="*" element={<ErrorPage />}></Route>
-      </Routes>
-    </AppProvider>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+        <Route path="/form" element={<FormPage />}></Route>
+      </Route>
+      <Route path="*" element={<ErrorPage />}></Route>
+    </Routes>
   );
 }
 
