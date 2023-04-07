68808e189a2703ed3963dd8f4950e6c9391e5aec
mldx <anton.zaprutski@gmail.com>
2023-04-07 15:02:23 +0300

    refactor: remove extra code

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,19 +1,10 @@
 import { createApi } from 'unsplash-js';
 
-export const ACCESS_KEY = 'xmx-M-AJ0UbvjxTASHIBeoh8WtViYJw0YiMzsTBbJwE';
+const ACCESS_KEY = 'xmx-M-AJ0UbvjxTASHIBeoh8WtViYJw0YiMzsTBbJwE';
 
-const api = createApi({
+export const api = createApi({
   accessKey: ACCESS_KEY,
 });
-
-export const getPhotos = async (query: string) => {
-  return await api.search.getPhotos({
-    query: query,
-    orientation: 'portrait',
-    perPage: 9,
-    page: 1,
-  });
-};
 
 export const getPhoto = async (id: string) => {
   return await api.photos.get({ photoId: id });
