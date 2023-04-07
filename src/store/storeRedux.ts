db0fdb7f9e52ce32bfc42c45568fccc934741057
mldx <anton.zaprutski@gmail.com>
2023-04-07 18:54:28 +0300

    feat: add imageCardSlice

--- src/store/storeRedux.ts
+++ src/store/storeRedux.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { orderCardsReducer } from './orderCardsSlice';
 import { searchBarReducer } from './searchBarSlice';
 import { imageCardsReducer } from './imageCardsSlice';
+import { imageCardReducer } from './imageCardSlice';
 
 export const store = configureStore({
   reducer: {
     orderCards: orderCardsReducer,
     searchBar: searchBarReducer,
     imageCards: imageCardsReducer,
+    imageCard: imageCardReducer,
   },
 });
 
